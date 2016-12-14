64e83636027340ce348f386228df10ad25c4ed24
MIchael Sørensen <mik2650@gmail.com>
2016-12-14 09:33:10 +0100

    Redirects to frontpage if valid redirect not set

--- pages/subscriber-reduced-price.php
+++ pages/subscriber-reduced-price.php
@@ -1,8 +1,15 @@
 <?php
 
+use Bonnier\WP\WypeResetpassword\Plugin;
 use Bonnier\WP\WypeResetpassword\Http\Routes\SubscriberReducedPricePage;
 
 include __DIR__ . '/partials/head.php';
+
+if(!Plugin::instance()->settings->get_setting_value('subscriber_valid_redirect_url'))
+{
+    wp_redirect( home_url() );
+    exit;
+}
 
 if(!SubscriberReducedPricePage::isAuthenticated()) {
     include __DIR__ . '/partials/subscripber-reduced-price/authenticateSubscriber.php';
